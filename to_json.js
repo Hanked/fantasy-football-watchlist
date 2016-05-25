1c74d94fe0a94de1a46e360024bec9c38a0f3caa
Hanked <dave.kingsnorth@photobox.com>
2016-05-25 15:42:52 +0100

    refactor to_json script to use path and read packages instead of fs

--- to_json.js
+++ to_json.js
@@ -1,27 +1,17 @@
-var Xray = require('x-ray');
-var x = Xray();
+var path = require('path')
+var read = require('fs').readFileSync
+var html = read(path.resolve(__dirname, 'html-snapshots/stats-table-7515.html'))
+var Xray = require('x-ray')
+var x = Xray()
 
-var fs = require('fs');
-
-// get contents of html file
-function readContent(callback) {
-  fs.readFile('./html-snapshots/stats-table-7515.html', 'utf-8', function (err, content) {
-    if (err) return callback(err)
-    callback(null, content)
-  })
-}
-
-var html;
-readContent(function (err, content) {
-  x(content, 'tbody tr',
-  [{
-    name: 'td.first',
-    team: 'td:nth-child(3)',
-    big_chances: 'td:nth-child(4)',
-    chances_created: 'td:nth-child(5)',
-    shots_inside_box: 'td:nth-child(11)',
-    shots_on_target: 'td:nth-child(12)',
-    touches_penalty_area: 'td:nth-child(13)'
-  }])
-  .write('results.json');
-})
+x(html, 'tbody tr',
+[{
+  name: 'td.first',
+  team: 'td:nth-child(3)',
+  big_chances: 'td:nth-child(4)',
+  chances_created: 'td:nth-child(5)',
+  shots_inside_box: 'td:nth-child(11)',
+  shots_on_target: 'td:nth-child(12)',
+  touches_penalty_area: 'td:nth-child(13)'
+}])
+.write('results.json');
